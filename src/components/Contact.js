aa35b935f176fcf2e5df7518f420b8ba3e80e3e8
lilmilo2704 <dangthienbaominh@gmail.com>
2026-02-19 20:44:27 +0700

    Connect frontend to deployed backend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,7 +28,7 @@ export const Contact = () => {
     setButtonText("Sending...");
 
     try {
-      const response = await fetch("http://localhost:5000/contact", {
+      const response = await fetch("https://portfolio-website-backend-zenq.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
