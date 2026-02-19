ee77f7dd8bd6eae5e181f847c89e08064d0c6e03
lilmilo2704 <dangthienbaominh@gmail.com>
2026-02-19 21:36:47 +0700

    fix nodemailer IPv6 to IPv4

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
